e948108cd5b66f76eaaafa7ca74156974ea66385
aknuck <arknw229@gmail.com>
2016-05-21 22:29:50 -0400

    just fukin work

--- php/signUp.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start(); // Starting Session
-$url = 'https://meet-up-1097.appspot.com/?command=makeUser&args='.$_POST['name'].';'.$_POST['email'].';'.$_POST['password'].'&token='.$_SESSION['token'];
-$url = str_replace(' ', "%20", $url);
-$result = file_get_contents($url);
-
-?>
--- readme.txt
+++
@@ -1,3 +0,0 @@
-CC Liscence Attributions:
-
-http://www.iconarchive.com/show/flatwoken-icons-by-alecive/Apps-Google-Maps-icon.html
