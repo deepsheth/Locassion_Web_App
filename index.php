c15915339ad0ec27285bd3c6db518a8c8ef93fb1
aknuck <arknw229@gmail.com>
2016-05-23 01:13:52 -0400

    updated index to new server changes

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ include('php/login.php');
             //Private Events
             var privateEventInfo = <?php
             if(isset($_SESSION['token'])){
-                $url = 'https://meet-up-1097.appspot.com/?command=getPrivateEvents&args=-75.375634;40.606709;2;2014-11-13%2016:00:00;2016-11-13%2016:00:00;:::&token='.$_SESSION['token'];
+                $url = 'https://meet-up-1097.appspot.com/?command=privateEvents&args=-75.375634;40.606709;2;2014-11-13%2016:00:00;2016-11-13%2016:00:00;:::&token='.$_SESSION['token'];
 
                 if (strpos(get_headers($url)[0],'200') != false){
                     $jsonResponse = json_decode(file_get_contents($url),true);
@@ -69,11 +69,12 @@ include('php/login.php');
             // 40.606709
             var publicEventInfo = <?php
             //if(isset($_SESSION['token'])){
-                $url = 'https://meet-up-1097.appspot.com/?command=getPublicEvents&args=-75.375634;40.606709;2;2014-11-13%2016:00:00;2016-11-13%2016:00:00;:::&token=';//.$_SESSION['token'];
+                $url = 'https://meet-up-1097.appspot.com/?command=publicEvents&args=-75.375634;40.606709;2;2014-11-13%2016:00:00;2016-11-13%2016:00:00;:::&token=';//.$_SESSION['token'];
                 if (strpos(get_headers($url)[0],'200') != false){
                     $jsonResponse = json_decode(file_get_contents($url),true);
                     $formattedIDs = implode(':',$jsonResponse['events']);
-                    $url = 'https://meet-up-1097.appspot.com/?command=getPublicEventInfo&args='.$formattedIDs.'&token=';//.$_SESSION['token'];
+                    $url = 'https://meet-up-1097.appspot.com/?command=getPublicEventInfo&args='.$formattedIDs.'&token=';
+                    //.$_SESSION['token'];
                     echo(file_get_contents($url).';//');
                     //echo('//'.$url);
                 }
